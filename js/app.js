3e69460754c276072c6d45ec0686f54f9accfb0d
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-02-20 15:04:33 +0530

    theme fixes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
 /* Theme toggle */
-const themeIcon = document.querySelector('.theme-icon');
 const themeStyle = document.querySelector('.themestyle');
-themeIcon.onclick = () => {
-        if (themeStyle.href.match("https://deploy-preview-5--card-input.netlify.app/darktheme.css")) {
-            themeStyle.setAttribute("href",
-                "https://deploy-preview-5--card-input.netlify.app/lighttheme.css");
-            themeIcon.classList.remove('fa-moon');
-            themeIcon.classList.add('fa-sun');
-        } else if (themeStyle.href.match("https://deploy-preview-5--card-input.netlify.app/lighttheme.css")) {
-            themeStyle.setAttribute("href", "https://deploy-preview-5--card-input.netlify.app/darktheme.css");
-            themeIcon.classList.remove('fa-sun');
-            themeIcon.classList.add('fa-moon');
-        }
-    }
+let activeSheet = document.querySelector("#active-stylesheet");
+let themeToggle = document.querySelector(".theme-toggle");
+let themeIcon = document.querySelector('.theme-toggle i');
 
-    /* JS for closing components */
-    const elements = document.querySelectorAll(".component-close");
+if (localStorage.getItem("lastActiveSheet")) {
+    activeSheet.setAttribute("href", localStorage.getItem("lastActiveSheet"));
+}
+
+function switchStyle() {
+    let selectedSheet = this.getAttribute("data-stylesheet");
+    console.log(selectedSheet);
+    if (selectedSheet === "https://deploy-preview-5--card-input.netlify.app/lighttheme.css") {
+        selectedSheet = "https://deploy-preview-5--card-input.netlify.app/darktheme.css";
+    } else {
+        selectedSheet = "https://deploy-preview-5--card-input.netlify.app/lighttheme.css";
+    }
+    this.setAttribute("data-stylesheet", selectedSheet);
+    activeSheet.setAttribute("href", selectedSheet);
+    localStorage.setItem("lastActiveSheet", selectedSheet);
+    if(themeIcon.classList.contains('fa-moon')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    }
+    else{
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+}
+
+// for (let i = 0; i < themeIcons.length; i++) {
+//     themeIcons[i].addEventListener("click", switchStyle);  
+// }
+
+themeToggle.addEventListener('click', switchStyle);
+
+/* JS for closing components */
+const elements = document.querySelectorAll(".component-close");
 for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", (e) => {
         e.target.parentElement.parentElement.style.display = "none";
